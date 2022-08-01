14a37aeb283a7aa36f88b724ae1a7ff311559d9f
Vimalraj <vimalrajvjkr@gmail.com>
2022-08-01 11:44:35 +0530

    Provider unit test - Orders

---
+++ test/provider/orders_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shopping_app/app/provider/cart.dart';
+import 'package:shopping_app/app/provider/orders.dart';
+
+void main() async {
+  Orders orders = Orders();
+  List<CartItem> cartItem = [];
+
+  setUp(() {
+    // given
+    cartItem.clear();
+    cartItem.add(CartItem("id", "title", 1, 34.0));
+  });
+
+  group("add Orders", () {
+    test(
+        "given order is added when order count is called then it returns total order count",
+        () async {
+      // when
+      orders.addOrders(cartItem, 109);
+
+      // then
+      expect(orders.ordersCount != 0, true);
+    });
+
+    test(
+        "given order is added when get orders is called then it returns total order list",
+        () async {
+      // when
+      orders.addOrders(cartItem, 109);
+
+      // then
+      expect(orders.orders.isNotEmpty, true);
+    });
+  });
+}
