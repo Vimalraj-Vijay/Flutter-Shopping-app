b32a317f8c33cdc2c0fe6d7b5899cd4136367acc
Vimalraj <vimalrajvjkr@gmail.com>
2022-08-04 16:38:30 +0530

    Code Formatting

--- android/app/src/main/kotlin/com/nodomain/shoppingapp/shopping_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/nodomain/shoppingapp/shopping_app/MainActivity.kt
@@ -2,5 +2,4 @@ package com.nodomain.shoppingapp.shopping_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
